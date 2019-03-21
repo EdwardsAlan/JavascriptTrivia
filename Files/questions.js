2685c327d1cba1a4df2ec87dfdaeb0c453990dc6
EdwardsAlan <alan.edwards1103@gmail.com>
2019-03-21 00:00:35 -0400

    Create questions.js

---
+++ Files/questions.js
@@ -0,0 +1,12 @@
+//creating an array of question objects
+function Question(text, choices, answer) {
+    this.text=text;
+    this.choices = choices;
+    this.answer = answer;
+
+}
+
+Question.prototype.correctAnswer = function(choice) {
+    return choice === this.answer;
+}
+
